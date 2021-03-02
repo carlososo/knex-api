39360214fb96ea13fd91cf377ed8e2f50eabb5a5
Carlos <carlosoc@live.com>
2021-03-01 20:10:01 -0600

    password and rol added

--- migrations/20210301195414_add_password_user.js
+++ migrations/20210301195414_add_password_user.js
@@ -1,6 +1,9 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTableIfNotExists('user',(table)=>{
+        table.string('password', 255).nullable();
+        table.string('rol').nullable();
+    });
 };
 
 exports.down = function(knex) {
